51a0c144640655a4e0d0c048d40d44f36f9e138c
erikhingl <168060840+erikhingl@users.noreply.github.com>
2024-05-09 09:57:28 +0200

    Update PythonApplication2.py

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -6,20 +6,37 @@ def generate_stats():
     stats = {
         'Strength': random.randint(3, 18),
         'Dexterity': random.randint(3, 18),
+        #ügyesség
         'Constitution': random.randint(3, 18),
+        #állóképesség
         'Intelligence': random.randint(3, 18),
         'Wisdom': random.randint(3, 18),
+        #bölcsesség
         'Charisma': random.randint(3, 18)
     }
     return stats
 
 def apply_race_bonus(race):
-    if race == 'Night Elf':
-        return {'Dexterity': 2}
-    elif race == 'Human':
+    if race == 'Human':
         return {stat: 1 for stat in ['Strength', 'Dexterity', 'Constitution', 'Intelligence', 'Wisdom', 'Charisma']}
-    else:
-        return {}
+    elif race == 'Elf':
+         return {'Dexterity': 2}
+     elif race == 'High Elf':
+         return {'Intelligence': 1}
+     elif race == 'Wood Elf':
+         return {'Intelligence': 1}
+     elif race == 'Halflings':
+         return {'Dexterity': 2}
+     elif race == 'Lightfoot':
+         return {'Charisma': 1}
+     elif race == 'Stout':
+         return {'Constitution': 1}
+     elif race == 'Dwarf':
+         return {'Constitution': 2}
+     elif race == 'Mountain Dwarf':
+         return {'Strength': 2}
+     else:
+         return{}
 
 def create_character_sheet(name, character_class, race):
     stats = generate_stats()
